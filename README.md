89525ee83700fe4a4afa9a47091e31def0efd80f
JastinXyz <jastinlinggar@gmail.com>
2023-01-28 17:10:31 +0700

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# how to
+
+- download kode dalam zip
+- ekstrak zip nya
+- pergi ke `chrome://extensions/` dan klik "Load unpacked"
+- pilih folder hasil ekstrak tadi untuk di import sebagai ekstensi
+- enjoy
---
+++ content.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", function() {
+    var loginform = document.getElementsByClassName("css-1c77zrq")[0];
+    var ans = document.querySelector(".question-content.css-zmelcr");
+
+    loginform.innerHTML = "";
+    loginform.classList.remove("css-1c77zrq");
+    
+    ans.style = "";
+    ans.classList.remove("css-zmelcr");
+}, false);
